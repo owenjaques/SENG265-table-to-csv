53141c75efdfcc407d87391f1ae4ab978756b828
owenjaques <owenjaques2000@gmail.com>
2019-11-26 14:52:15 -0800

    sorta sorts with tables

--- table_to_csv.py
+++ table_to_csv.py
@@ -3,19 +3,36 @@ import re
 
 def main():
 	#string to be parsed
-	s = input()
-	#removes the </tr...>
-	s = re.sub('</tr.*?>', '', s, flags=re.IGNORECASE | re.DOTALL)
-	#splits on <tr...>
-	s = re.split('<tr.*?>', s, flags=re.IGNORECASE | re.DOTALL)
-	#removes the first element of the list because it is always blank
+	s = ''
+	while True:
+		try:
+			s += input() + '\n'
+		except EOFError:
+			break
+
+	#replaces all white space including new lines with one space
+	s = re.sub('\s+', ' ', s, flags=re.IGNORECASE)
+
+	#removes the </table...>
+	s = re.sub('</table.*?>', '', s, flags=re.IGNORECASE)
+	#splits on <table...>
+	s = re.split('<table.*?>', s, flags=re.IGNORECASE)
+	#removes the first element of the list because it is always blank or useless
 	s = s[1:]
 	
-	#splits each table row up into its cells
-	for i in range(len(s)):
-		s[i] = re.sub('</td.*?>', '', s[i], flags=re.IGNORECASE | re.DOTALL)
-		s[i] = re.split('<td.*?>', s[i], flags=re.IGNORECASE | re.DOTALL)
-		s[i] = s[i][1:]
+	for j in range(len(s)):
+		#removes the </tr...>
+		s[j] = re.sub('</tr.*?>', '', s[j], flags=re.IGNORECASE)
+		#splits on <tr...>
+		s[j] = re.split('<tr.*?>', s[j], flags=re.IGNORECASE)
+		#removes the first element of the list because it is always blank or useless
+		s[j] = s[j][1:]
+		
+		#splits each table row up into its cells
+		for i in range(len(s[j])):
+			s[j][i] = re.sub('</td.*?>', '', s[j][i], flags=re.IGNORECASE)
+			s[j][i] = re.split('<td.*?>', s[j][i], flags=re.IGNORECASE)
+			s[j][i] = s[j][i][1:]
 
 	print(s)
 
